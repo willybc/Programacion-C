31f8ce1ca2a3077ba68006c0b081d4750067ef07
willybc <willybc06@gmail.com>
2018-11-11 21:18:06 -0300

    ej9 - Arrays Bidimensionales

--- arrays bidimensionales/ej9.c
+++ arrays bidimensionales/ej9.c
@@ -21,6 +21,17 @@ void emitir ( int f , int c , int m[][c]){
     }
 }
 
+void comparacion ( int f , int c ,int m1[][c] , int m2[][c]){
+    int i,j;
+    for(i=0 ; i<f ; i++){
+        for(j=0 ; j<c ; j++){
+            if( m1[i][j] == m2[i][j]){
+                printf("El elemento %d de la matriz 1 es comun a el elemento %d de la matriz 2 en la posicion [%d][%d]\n",m1[i][j],m2[i][j],i+1,j+1);
+            }
+        }
+    }
+}
+
 int main() {
 
     const int F1 = 2;
@@ -42,6 +53,8 @@ int main() {
     emitir(F1 , C1 , m1);
     printf("Matriz 2:\n");
     emitir(F2,C2, m2);
+
+    comparacion (F1,C1,m1,m2);
 
 }
 
